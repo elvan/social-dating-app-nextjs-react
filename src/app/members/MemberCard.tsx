ab3b38ae6af83c6ee0270f92f4616693d3259a72
Elvan Hidayat <elvanhd@gmail.com>
2024-06-02 14:59:00 +0700

    Add like functionality and member list tabs
    - Update schema.prisma to include Like model and relationships
    - Create likeActions to handle like and unlike operations
    - Add getAuthUserId function to retrieve the current user ID
    - Implement toggleLikeMember and fetchLikedMembers actions
    - Add ListsTab component to display liked members with tabs
    - Update ListsPage to fetch liked members and like IDs
    - Enhance MemberCard with LikeButton and like functionality
    - Create LikeButton component for liking/unliking members
    - Add LoadingComponent to display loading state

--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -1,13 +1,25 @@
+'use client';
+
+import LikeButton from '@/components/LikeButton';
 import { calculateAge } from '@/lib/util';
 import { Card, CardFooter, Image } from '@nextui-org/react';
 import { Member } from '@prisma/client';
 import Link from 'next/link';
+import React from 'react';
 
 type Props = {
   member: Member;
+  likeIds: string[];
 };
 
-export default function MemberCard({ member }: Props) {
+export default function MemberCard({ member, likeIds }: Props) {
+  const hasLiked = likeIds.includes(member.userId);
+
+  const preventLinkAction = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
   return (
     <Card fullWidth as={Link} href={`/members/${member.userId}`} isPressable>
       <Image
@@ -17,6 +29,12 @@ export default function MemberCard({ member }: Props) {
         src={member.image || '/images/user.png'}
         className='aspect-square object-cover'
       />
+      <div onClick={preventLinkAction}>
+        <div className='absolute top-3 right-3 z-50'>
+          <LikeButton targetId={member.userId} hasLiked={hasLiked} />
+        </div>
+      </div>
+
       <CardFooter className='flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient'>
         <div className='flex flex-col text-white'>
           <span className='font-semibold'>
---
+++ src/components/LoadingComponent.tsx
@@ -0,0 +1,9 @@
+import { Spinner } from '@nextui-org/react';
+
+export default function LoadingComponent({ label }: { label?: string }) {
+  return (
+    <div className='fixed inset-0 flex justify-center items-center'>
+      <Spinner label={label || 'Loading...'} color='secondary' labelColor='secondary' />
+    </div>
+  );
+}
